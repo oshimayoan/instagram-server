c2e0c99832b4cdc228fc4dcd6659dc3b5560a862
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2020-09-17 18:48:29 +0700

    fix relations and send highlighted comments

--- api/posts/controllers/posts.js
+++ api/posts/controllers/posts.js
@@ -5,4 +5,36 @@
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.posts.search(ctx.query);
+    } else {
+      entities = await strapi.services.posts.find(ctx.query);
+    }
+
+    let newEntities = [];
+    for (let i = 0; i < entities.length; i++) {
+      let relatedComments = await strapi.services.comments.find({postId: entities[i].id})
+
+      let highlightedComments = relatedComments.map(comment => ({
+        id: comment.id,
+        content: comment.content,
+        postId: comment.postId,
+        user: {
+          id: comment.user.id,
+          username: comment.user.username,
+        },
+        createdAt: comment.createdAt,
+      }));
+
+      let newEntity = { ...entities[i], highlightedComments };
+      newEntities.push(newEntity);
+    }
+
+    return newEntities.map(entity => sanitizeEntity(entity, { model: strapi.models.posts }));
+  }
+};
